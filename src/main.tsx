1bc091a853c8e92b8b47810930516b4d27df11c1
polaris0211 <junhee.park211@icloud.com>
2025-02-22 16:48:24 +0900

    Change font to Pretendard

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createTheme, ThemeProvider } from "@mui/material";
+import Pretendard from "../fonts/Pretendard.woff2";
+import Box from "@mui/material/Box";
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      "Pretendard",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      '"Segoe UI"',
+      "Roboto",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(","),
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: `
+              @font-face {
+	font-family: 'Pretendard';
+	font-weight: 45 920;
+	font-style: normal;
+	font-display: swap;
+	src: url('${Pretendard}') format('woff2-variations');
+}
+      `,
+    },
+  },
+});
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Box sx={{ fontFamily: "Pretendard" }}>
+        <App />
+      </Box>
+    </ThemeProvider>
+  </React.StrictMode>
+);
 
 // Use contextBridge
-window.ipcRenderer.on('main-process-message', (_event, message) => {
-  console.log(message)
-})
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
